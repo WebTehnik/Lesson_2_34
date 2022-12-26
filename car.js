e377cb7ac597165f3aebcbede1735a9d39dd953e
Yuri-PC\Dell5570 <webtehnik@gmail.com>
2022-12-26 21:52:32 +0100

    first commit

---
+++ car.js
@@ -0,0 +1,24 @@
+const carName = 'Дэу Ланос';
+
+let maxCarSpeed, carOwner;
+
+maxCarSpeed = '150 км/ч';
+carOwner = 'Юрий Чех';
+
+maxCarSpeed = '140 км/ч';
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = '130 км/ч';
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = '120 км/ч';
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+carOwner = 'Маргарита Чех';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Какой-то 18-летний парень';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Хозяин автошрота';
+console.log('У машины изменился владелец', carOwner);
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
